763f61b68d89e28fa177f22ccbd84230019f0baa
KorbKenny <korbkenny@gmail.com>
2016-12-09 15:10:54 -0500

    sign in

--- ScottAndKennyFirebaseLab/app/src/main/java/com/korbkenny/scottandkennyfirebaselab/MainActivity.java
+++ ScottAndKennyFirebaseLab/app/src/main/java/com/korbkenny/scottandkennyfirebaselab/MainActivity.java
@@ -1,16 +1,63 @@
 package com.korbkenny.scottandkennyfirebaselab;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ui.ResultCodes;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     public static final String CHATROOM_INTENT_KEY = "chatroom key";
+    private static final int RC_SIGN_IN = 12345;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .build();
 
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+
+        } else{
+            startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // user is signed in!
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                return;
+            }
+            if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "SIGN IN CANCELLED", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (resultCode == ResultCodes.RESULT_NO_NETWORK) {
+                Toast.makeText(this, "NO INTERNET!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
     }
 }
